ec5d57c9f6ae7950ef6c6febea951bc6be584e94
JohnyAls <techevo404@gmail.com>
2025-11-18 00:20:26 +0300

    rf- code on next-config

---
+++ next.config.js
@@ -0,0 +1,15 @@
+// next.config.js
+module.exports = {
+  async rewrites() {
+    const apiBase = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000";
+
+    return [
+      { source: "/storage/:path*", destination: `${apiBase}/storage/:path*` },
+      { source: "/sanctum/:path*", destination: `${apiBase}/sanctum/:path*` },
+      { source: "/api/:path*", destination: `${apiBase}/api/:path*` },
+
+      { source: "/login", destination: "/auth/login" },
+      { source: "/register", destination: "/auth/register" },
+    ];
+  },
+};
